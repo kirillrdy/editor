b63f2dfcfb9ebebe3dfae2587214973683fde5a3
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2016-08-26 16:27:48 +1000

    renamed and fixed up all the warnings

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,6 @@ use cairo::enums::{FontSlant, FontWeight};
 use cairo::Context;
 use gdk::enums::modifier_type;
 
-use std::sync::mpsc::sync_channel;
-use std::thread;
 use std::sync::Arc;
 use std::sync::Mutex;
 
@@ -24,10 +22,10 @@ fn main() {
     let window = gtk::Window::new(gtk::WindowType::Toplevel);
     let drawing_area = Box::new(DrawingArea::new)();
 
-    let mut data: Vec<String> = Vec::new();
-    let mut data = Arc::new(Mutex::new(data));
+    let buffer: Vec<String> = Vec::new();
+    let buffer = Arc::new(Mutex::new(buffer));
 
-    let for_drawing = data.clone();
+    let buffer_for_drawing = buffer.clone();
     drawing_area.connect_draw(move |_, cr| {
 
         cr.select_font_face("Mono", FontSlant::Normal, FontWeight::Normal);
@@ -37,10 +35,13 @@ fn main() {
         let mut row = 0;
         let mut column = 0;
 
-        let data = for_drawing.lock().unwrap();
+        let data = buffer_for_drawing.lock().unwrap();
         for single_letter in data.iter() {
             put_char(cr, row, column, single_letter.to_string());
             column = column + 1;
+            if single_letter == "\n" {
+                row = row + 1
+            }
         }
 
         println!("{}", time::now() - now);
@@ -54,11 +55,9 @@ fn main() {
         Inhibit(false)
     });
 
-    window.add(&drawing_area);
-    window.show_all();
 
-    let mut writer = data.clone();
-    let w = window.clone();
+    let writer = buffer.clone();
+    let window_to_redraw = window.clone();
     window.connect_key_press_event(move |_, key| {
         let keyval = key.as_ref().keyval;
         let keystate = key.as_ref().state;
@@ -69,17 +68,21 @@ fn main() {
         let key_pressed = keyval as u8 as char;
         let key_pressed = format!("{}", key_pressed);
         guy.push(key_pressed.clone());
-        w.queue_draw_area(0,0,100000,10000);
-        //println!("key pressed: {} / {:?}", keyval, keystate);
 
-        //if keystate.intersects(modifier_type::ControlMask) {
-        //    println!("You pressed Ctrl!");
-        //}
+        //TODO fix width etc
+        window_to_redraw.queue_draw_area(0,0,100000,10000);
+        println!("key pressed: {} / {:?}", keyval, keystate);
+
+        if keystate.intersects(modifier_type::ControlMask) {
+           println!("You pressed Ctrl!");
+        }
 
         Inhibit(false)
     });
 
 
+    window.add(&drawing_area);
+    window.show_all();
 
     gtk::main();
 }
